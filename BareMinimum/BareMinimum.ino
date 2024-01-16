0fcb00d0ebe1807b99f64e8ac7e77efaeda50b98
tejbho <tejbho305@gmail.com>
2024-01-15 22:21:52 -0800

    My first arduino circuit

---
+++ BareMinimum/BareMinimum.ino
@@ -0,0 +1,70 @@
+void setup() {
+  // put your setup code here, to run once:
+pinMode(13, OUTPUT);
+pinMode(2, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+digitalWrite(13, HIGH);
+delay(100);
+digitalWrite(13,LOW);
+delay(100);
+digitalWrite(13, HIGH);
+delay(100);
+digitalWrite(13,LOW);
+delay(100);
+digitalWrite(13, HIGH);
+delay(100);
+digitalWrite(13,LOW);
+delay(100);
+digitalWrite(13, HIGH);
+delay(100);
+digitalWrite(13,LOW);
+delay(100);
+digitalWrite(13, HIGH);
+delay(100);
+digitalWrite(13,LOW);
+delay(100);
+
+digitalWrite(2, HIGH);
+delay(100);
+digitalWrite(2,LOW);
+delay(100);
+digitalWrite(2, HIGH);
+delay(100);
+digitalWrite(2,LOW);
+delay(100);
+digitalWrite(2, HIGH);
+delay(100);
+digitalWrite(2,LOW);
+delay(100);
+digitalWrite(2, HIGH);
+delay(100);
+digitalWrite(2,LOW);
+delay(100);
+digitalWrite(2, HIGH);
+delay(100);
+digitalWrite(2,LOW);
+delay(100);
+digitalWrite(2, HIGH);
+delay(100);
+digitalWrite(2,LOW);
+delay(100);
+digitalWrite(2, HIGH);
+delay(100);
+digitalWrite(2,LOW);
+delay(100);
+digitalWrite(2, HIGH);
+delay(100);
+digitalWrite(2,LOW);
+delay(100);
+digitalWrite(2, HIGH);
+delay(100);
+digitalWrite(2,LOW);
+delay(100);
+digitalWrite(2, HIGH);
+delay(100);
+digitalWrite(2,LOW);
+delay(100);
+}
