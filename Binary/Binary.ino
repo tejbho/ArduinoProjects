e31845aa2008c7e64ae8e3f8a7dbb5c14f563d9b
tejbho <tejbho305@gmail.com>
2024-03-30 17:29:14 -0700

    Binary LEDs

---
+++ Binary/Binary.ino
@@ -0,0 +1,181 @@
+int redPin=13;
+int greenPin=2;
+int bluePin=3;
+int yellowPin=4;
+int wait=1000;
+
+void setup() {
+  // put your setup code here, to run once:
+pinMode(redPin, OUTPUT);
+pinMode(greenPin, OUTPUT);
+pinMode(bluePin, OUTPUT);
+pinMode(yellowPin, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+digitalWrite(redPin, LOW);
+digitalWrite(greenPin, LOW);
+digitalWrite(bluePin, LOW);
+digitalWrite(yellowPin, HIGH);
+delay(wait);
+digitalWrite(redPin, LOW);
+digitalWrite(greenPin, LOW);
+digitalWrite(bluePin, LOW);
+digitalWrite(yellowPin, LOW);
+delay(wait);
+
+digitalWrite(redPin, LOW);
+digitalWrite(greenPin, LOW);
+digitalWrite(bluePin, HIGH);
+digitalWrite(yellowPin, LOW);
+delay(wait);
+digitalWrite(redPin, LOW);
+digitalWrite(greenPin, LOW);
+digitalWrite(bluePin, LOW);
+digitalWrite(yellowPin, LOW);
+delay(wait);
+
+digitalWrite(redPin, LOW);
+digitalWrite(greenPin, LOW);
+digitalWrite(bluePin, HIGH);
+digitalWrite(yellowPin, HIGH);
+delay(wait);
+digitalWrite(redPin, LOW);
+digitalWrite(greenPin, LOW);
+digitalWrite(bluePin, LOW);
+digitalWrite(yellowPin, LOW);
+delay(wait);
+
+digitalWrite(redPin, LOW);
+digitalWrite(greenPin, HIGH);
+digitalWrite(bluePin, LOW);
+digitalWrite(yellowPin, LOW);
+delay(wait);
+digitalWrite(redPin, LOW);
+digitalWrite(greenPin, LOW);
+digitalWrite(bluePin, LOW);
+digitalWrite(yellowPin, LOW);
+delay(wait);
+
+digitalWrite(redPin, LOW);
+digitalWrite(greenPin, HIGH);
+digitalWrite(bluePin, LOW);
+digitalWrite(yellowPin, HIGH);
+delay(wait);
+digitalWrite(redPin, LOW);
+digitalWrite(greenPin, LOW);
+digitalWrite(bluePin, LOW);
+digitalWrite(yellowPin, LOW);
+delay(wait);
+
+digitalWrite(redPin, LOW);
+digitalWrite(greenPin, HIGH);
+digitalWrite(bluePin, HIGH);
+digitalWrite(yellowPin, LOW);
+delay(wait);
+digitalWrite(redPin, LOW);
+digitalWrite(greenPin, LOW);
+digitalWrite(bluePin, LOW);
+digitalWrite(yellowPin, LOW);
+delay(wait);
+
+digitalWrite(redPin, LOW);
+digitalWrite(greenPin, HIGH);
+digitalWrite(bluePin, HIGH);
+digitalWrite(yellowPin, HIGH);
+delay(wait);
+digitalWrite(redPin, LOW);
+digitalWrite(greenPin, LOW);
+digitalWrite(bluePin, LOW);
+digitalWrite(yellowPin, LOW);
+delay(wait);
+
+digitalWrite(redPin, HIGH);
+digitalWrite(greenPin, LOW);
+digitalWrite(bluePin, LOW);
+digitalWrite(yellowPin, LOW);
+delay(wait);
+digitalWrite(redPin, LOW);
+digitalWrite(greenPin, LOW);
+digitalWrite(bluePin, LOW);
+digitalWrite(yellowPin, LOW);
+delay(wait);
+
+digitalWrite(redPin, HIGH);
+digitalWrite(greenPin, LOW);
+digitalWrite(bluePin, LOW);
+digitalWrite(yellowPin, HIGH);
+delay(wait);
+digitalWrite(redPin, LOW);
+digitalWrite(greenPin, LOW);
+digitalWrite(bluePin, LOW);
+digitalWrite(yellowPin, LOW);
+delay(wait);
+
+digitalWrite(redPin, HIGH);
+digitalWrite(greenPin, LOW);
+digitalWrite(bluePin, HIGH);
+digitalWrite(yellowPin, LOW);
+delay(wait);
+digitalWrite(redPin, LOW);
+digitalWrite(greenPin, LOW);
+digitalWrite(bluePin, LOW);
+digitalWrite(yellowPin, LOW);
+delay(wait);
+
+digitalWrite(redPin, HIGH);
+digitalWrite(greenPin, LOW);
+digitalWrite(bluePin, HIGH);
+digitalWrite(yellowPin, HIGH);
+delay(wait);
+digitalWrite(redPin, LOW);
+digitalWrite(greenPin, LOW);
+digitalWrite(bluePin, LOW);
+digitalWrite(yellowPin, LOW);
+delay(wait);
+
+digitalWrite(redPin, HIGH);
+digitalWrite(greenPin, HIGH);
+digitalWrite(bluePin, LOW);
+digitalWrite(yellowPin, LOW);
+delay(wait);
+digitalWrite(redPin, LOW);
+digitalWrite(greenPin, LOW);
+digitalWrite(bluePin, LOW);
+digitalWrite(yellowPin, LOW);
+delay(wait);
+
+digitalWrite(redPin, HIGH);
+digitalWrite(greenPin, HIGH);
+digitalWrite(bluePin, LOW);
+digitalWrite(yellowPin, HIGH);
+delay(wait);
+digitalWrite(redPin, LOW);
+digitalWrite(greenPin, LOW);
+digitalWrite(bluePin, LOW);
+digitalWrite(yellowPin, LOW);
+delay(wait);
+
+digitalWrite(redPin, HIGH);
+digitalWrite(greenPin, HIGH);
+digitalWrite(bluePin, HIGH);
+digitalWrite(yellowPin, LOW);
+delay(wait);
+digitalWrite(redPin, LOW);
+digitalWrite(greenPin, LOW);
+digitalWrite(bluePin, LOW);
+digitalWrite(yellowPin, LOW);
+delay(wait);
+
+digitalWrite(redPin, HIGH);
+digitalWrite(greenPin, HIGH);
+digitalWrite(bluePin, HIGH);
+digitalWrite(yellowPin, HIGH);
+delay(wait);
+digitalWrite(redPin, LOW);
+digitalWrite(greenPin, LOW);
+digitalWrite(bluePin, LOW);
+digitalWrite(yellowPin, LOW);
+delay(wait);
+}
