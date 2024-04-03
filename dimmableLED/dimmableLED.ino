74ec2893ddab787e8d21f241cc40c3be1e22158f
tejbho <tejbho305@gmail.com>
2024-04-02 17:21:40 -0700

    dimmable light with potentiometer

---
+++ dimmableLED/dimmableLED.ino
@@ -0,0 +1,20 @@
+int potPin=A2;
+int gPin=3;
+int potVal;
+float LEDVal;
+
+void setup() {
+  // put your setup code here, to run once:
+pinMode(potPin, INPUT);
+pinMode(gPin, OUTPUT);
+Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+potVal=analogRead(potPin);
+LEDVal=(255./1023.)*potVal;
+analogWrite(gPin,LEDVal);
+Serial.println(LEDVal);
+delay(500);
+}
